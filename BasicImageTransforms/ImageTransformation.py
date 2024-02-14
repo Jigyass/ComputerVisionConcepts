9e80d286ea76acfaf9e08bc747ffaf34baabffb0
Jigyass <jigyas@icloud.com>
2024-02-13 20:06:21 -0600

    Fixed Issue in python code

--- BasicImageTransforms/ImageTransformation.py
+++ BasicImageTransforms/ImageTransformation.py
@@ -17,6 +17,7 @@ from PIL import Image
 def flipflopim(image):
     flipped_image = image [:, ::-1] #Using python's built in slicing functionality, we select all the rows to be in the same order, however the columns are selected in reverse order using -1
     flipfloppedimage = flipped_image[::-1, :] #Using python's slicing ability to flip the rows in the array while keeping the columns same since the passed image is already flipped
+    return flipfloppedimage
 
 '''
 @param: None
