77e0df4721c9270fa99e13770ec8e62e5dc15f9c
Jigyass <jigyas@icloud.com>
2024-03-05 11:32:30 -0600

    Path for mac

--- HistogramTransformation/Histogram_Transformation.py
+++ HistogramTransformation/Histogram_Transformation.py
@@ -72,7 +72,13 @@ def RunHistogram(image_path, title):
     PlotHistogram(equalized, "Equalized")
     plt.show()
 
+RunHistogram('/Users/darksst/Desktop/Github/ComputerVisionConcepts/HistogramTransformation/swan.pgm', 'SWAN')
+
+RunHistogram('/Users/darksst/Desktop/Github/ComputerVisionConcepts/HistogramTransformation/tools.pgm', 'TOOLS')
 
 
+'''
 RunHistogram('/home/darksst/Desktop/ComputerVisionConcepts/HistogramTransformation/swan.pgm', 'SWAN')
 RunHistogram('/home/darksst/Desktop/ComputerVisionConcepts/HistogramTransformation/tools.pgm', 'TOOLS')
+'''
+
